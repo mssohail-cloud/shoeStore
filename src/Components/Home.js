83e0d8a8427e0d0ca955bfbd4894381570350fdc
Muhammad Sohail <muhammadsohail87@gmail.com>
2020-09-07 07:22:03 +0500

    removed unused imports

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Link } from '@material-ui/core'
-import ProductList from './ProductList'
-import { Routes, Route } from 'react-router'
+
+
 
 function Home() {
     return (
         <div>
-            <Routes>
-                <Route path="ProductList" element={<ProductList />} />
-            </Routes>
             <h1>Welcome to our store, See our collection in products section</h1>
         </div>
     )
